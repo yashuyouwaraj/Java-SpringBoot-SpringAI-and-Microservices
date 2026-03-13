0725d5c6fe8ed0a3a63f9f9ab05ad3e4e55f85c3
Yashu Youwaraj <amanyasu@gmail.com>
2026-03-13 11:06:11 +0530

    Core Java => Variables and DataTypes

---
+++ 1.Core Java/DataTypes.java
@@ -0,0 +1,22 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int num1= 121;
+        byte num2 = 127;
+        short num3 = 32767;
+        long num4 = 123456789L;
+
+        float f = 50.5f;
+        double d = 123.456;
+
+        char c = 'A';
+        boolean b = true;
+        System.out.println("Integer: " + num1);
+        System.out.println("Byte: " + num2);
+        System.out.println("short: " + num2);
+        System.out.println("long: " + num2);
+        System.out.println("float: " + num2);
+        System.out.println("double: " + num2);
+        System.out.println("char: " + c);
+        System.out.println("boolean: " + b);
+    }
+}
---
+++ 1.Core Java/Varibles.java
@@ -0,0 +1,9 @@
+public class Varibles {
+
+    public static void main(String args[]){
+        int a = 10;
+        int b= 20;
+        int c=a+b;
+        System.out.println("The value of a+b is: "+ c);
+    }
+}
