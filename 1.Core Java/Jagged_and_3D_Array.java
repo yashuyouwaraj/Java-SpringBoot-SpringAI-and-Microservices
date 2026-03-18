eacd550f61677daac98021beb94cb5caeecc3b41
Yashu Youwaraj <amanyasu@gmail.com>
2026-03-18 10:37:25 +0530

    Core Java => Jagged and 3D Arrays

---
+++ 1.Core Java/Jagged_and_3D_Array.java
@@ -0,0 +1,24 @@
+public class Jagged_and_3D_Array {
+    public static void main(String[] args) {
+        int nums[][]=new int[3][]; //jagged array
+        nums[0]=new int[3];
+        nums[1]=new int[4];
+        nums[2]=new int[2];
+
+        int nums1[][]=new int[3][4];
+        int nums2[][][]=new int[2][3][4]; //three dimensional array
+
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<nums[i].length;j++){
+                nums[i][j]=(int)(Math.random()*10);
+            }
+        }
+
+        for (int n[] :nums){
+            for(int m:n){
+                System.out.print(m+" ");
+            }
+            System.out.println();
+        }
+    }
+}
