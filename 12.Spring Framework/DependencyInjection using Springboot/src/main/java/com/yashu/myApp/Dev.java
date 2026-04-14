9c5268fd25d7a182c5c8c4f71596a7f8bd16df56
Yashu Youwaraj <amanyasu@gmail.com>
2026-04-14 14:10:45 +0530

    Spring Framework => Dependency Injection

---
+++ 12.Spring Framework/DependencyInjection using Springboot/src/main/java/com/yashu/myApp/Dev.java
@@ -0,0 +1,11 @@
+package com.yashu.myApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+    public void build(){
+        System.out.println("Working on Awesome Project");
+
+    }
+}
